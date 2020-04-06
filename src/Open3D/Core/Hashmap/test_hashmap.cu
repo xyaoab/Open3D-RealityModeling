4098140a9ee2960546ef6a42203b129d32f4917d
Wei Dong <weidong@andrew.cmu.edu>
2020-04-05 22:13:53 -0400

    re-support other tests for test_hashmap

--- src/Open3D/Core/Hashmap/test_hashmap.cu
+++ src/Open3D/Core/Hashmap/test_hashmap.cu
@@ -88,7 +88,7 @@ struct hash<Vector6i> {
         return h;
     }
 };
-}
+}  // namespace std
 
 void TEST_SIMPLE() {
     std::unordered_map<int, int> unordered_map;
@@ -119,7 +119,8 @@ void TEST_SIMPLE() {
         } else {
             iterator_t iterator = cuda_query_results.first[i];
             int key = *(thrust::device_ptr<int>((int*)iterator));
-            int val = *(thrust::device_ptr<int>((int*)(iterator + sizeof(int))));
+            int val =
+                    *(thrust::device_ptr<int>((int*)(iterator + sizeof(int))));
             std::cout << key << " " << val << "\n";
             assert(key == cuda_query_keys[i]);
             assert(val == iter->second);
@@ -129,156 +130,155 @@ void TEST_SIMPLE() {
     std::cout << "TEST_SIMPLE() passed\n";
 }
 
-// void TEST_6DIM_KEYS(int key_size) {
-//     std::default_random_engine generator;
-//     std::uniform_int_distribution<int> dist(-1000, 1000);
+void TEST_6DIM_KEYS(int key_size) {
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> dist(-1000, 1000);
 
-//     // generate data
-//     std::cout << "generating data...\n";
-//     std::vector<Vector6i> insert_keys(key_size);
-//     std::vector<int> insert_vals(key_size);
-//     for (int i = 0; i < key_size; ++i) {
-//         insert_keys[i].Random_(generator, dist);
-//         insert_vals[i] = i;
-//     }
-//     thrust::device_vector<Vector6i> cuda_insert_keys = insert_keys;
-//     thrust::device_vector<int> cuda_insert_vals = insert_vals;
-//     std::cout << "data generated\n";
+    // generate data
+    std::cout << "generating data...\n";
+    std::vector<Vector6i> insert_keys(key_size);
+    std::vector<int> insert_vals(key_size);
+    for (int i = 0; i < key_size; ++i) {
+        insert_keys[i].Random_(generator, dist);
+        insert_vals[i] = i;
+    }
+    thrust::device_vector<Vector6i> cuda_insert_keys = insert_keys;
+    thrust::device_vector<int> cuda_insert_vals = insert_vals;
+    std::cout << "data generated\n";
 
-//     // cpu groundtruth
-//     std::cout << "generating std::unordered_map ground truth...\n";
-//     std::unordered_map<Vector6i, int> unordered_map;
-//     for (int i = 0; i < key_size; ++i) {
-//         unordered_map[insert_keys[i]] = insert_vals[i];
-//     }
-//     std::cout << "ground truth generated\n";
+    // cpu groundtruth
+    std::cout << "generating std::unordered_map ground truth...\n";
+    std::unordered_map<Vector6i, int> unordered_map;
+    for (int i = 0; i < key_size; ++i) {
+        unordered_map[insert_keys[i]] = insert_vals[i];
+    }
+    std::cout << "ground truth generated\n";
 
-//     // gpu test
-//     std::cout << "inserting to cuda::Hashmap...\n";
-//     cuda::Hashmap<Vector6i, int> cuda_unordered_map(key_size);
-//     cuda_unordered_map.Insert(cuda_insert_keys, cuda_insert_vals);
-//     std::cout << "insertion finished\n";
+    // gpu test
+    std::cout << "inserting to cuda::Hashmap...\n";
+    cuda::Hashmap<Vector6i, int> cuda_unordered_map(key_size);
+    cuda_unordered_map.Insert(cuda_insert_keys, cuda_insert_vals);
+    std::cout << "insertion finished\n";
 
-//     // query -- all true
-//     std::cout << "generating query_data...\n";
-//     thrust::device_vector<Vector6i> cuda_query_keys(insert_keys.size());
-//     for (int i = 0; i < key_size; ++i) {
-//         if (i % 3 == 2) {
-//             cuda_query_keys[i] = cuda_insert_keys[i];
-//         } else {
-//             cuda_query_keys[i] = Vector6i().Random_(generator, dist);
-//         }
-//     }
-//     std::cout << "query data generated\n";
+    // query -- all true
+    std::cout << "generating query_data...\n";
+    thrust::device_vector<Vector6i> cuda_query_keys(insert_keys.size());
+    for (int i = 0; i < key_size; ++i) {
+        if (i % 3 == 2) {
+            cuda_query_keys[i] = cuda_insert_keys[i];
+        } else {
+            cuda_query_keys[i] = Vector6i().Random_(generator, dist);
+        }
+    }
+    std::cout << "query data generated\n";
 
-//     std::cout << "query from cuda::Hashmap...\n";
-//     auto cuda_query_results = cuda_unordered_map.Search(cuda_query_keys);
-//     std::cout << "query results generated\n";
+    std::cout << "query from cuda::Hashmap...\n";
+    auto cuda_query_results = cuda_unordered_map.Search(cuda_query_keys);
+    std::cout << "query results generated\n";
 
-//     std::cout << "comparing query results against ground truth...\n";
-//     for (int i = 0; i < cuda_query_keys.size(); ++i) {
-//         auto iter = unordered_map.find(cuda_query_keys[i]);
-//         if (iter == unordered_map.end()) {
-//             assert(cuda_query_results.second[i] == 0);
-//         } else {
-//             Iterator<Vector6i, int> iterator = cuda_query_results.first[i];
-//             // Iterator == Pair*
-//             Pair<Vector6i, int> kv =
-//                     *(thrust::device_ptr<Pair<Vector6i, int>>(iterator));
-//             assert(kv.first == cuda_query_keys[i]);
-//             assert(kv.second == iter->second);
-//         }
-//     }
+    std::cout << "comparing query results against ground truth...\n";
+    for (int i = 0; i < cuda_query_keys.size(); ++i) {
+        auto iter = unordered_map.find(cuda_query_keys[i]);
+        if (iter == unordered_map.end()) {
+            assert(cuda_query_results.second[i] == 0);
+        } else {
+            iterator_t iterator = cuda_query_results.first[i];
+            Vector6i key = *(thrust::device_ptr<Vector6i>((Vector6i*)iterator));
+            int val = *(thrust::device_ptr<int>(
+                    (int*)(iterator + sizeof(Vector6i))));
+            assert(key == cuda_query_keys[i]);
+            assert(val == iter->second);
+        }
+    }
 
-//     std::cout << "TEST_6DIM_KEYS() passed\n";
-// }
+    std::cout << "TEST_6DIM_KEYS() passed\n";
+}
 
+void TEST_COORD_KEYS(int key_size) {
+    const int D = 8;
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> dist(-1000, 1000);
 
-// void TEST_COORD_KEYS(int key_size) {
-//     const int D = 8;
-//     std::default_random_engine generator;
-//     std::uniform_int_distribution<int> dist(-1000, 1000);
+    // generate raw data (a bunch of data mimicking at::Tensor)
+    std::cout << "generating data...\n";
+    std::vector<int> input_coords(key_size * D);
+    for (int i = 0; i < key_size * D; ++i) {
+        input_coords[i] = dist(generator);
+    }
+    std::cout << "data generated\n";
 
-//     // generate raw data (a bunch of data mimicking at::Tensor)
-//     std::cout << "generating data...\n";
-//     std::vector<int> input_coords(key_size * D);
-//     for (int i = 0; i < key_size * D; ++i) {
-//         input_coords[i] = dist(generator);
-//     }
-//     std::cout << "data generated\n";
+    // convert raw data (at::Tensor) to std::vector
+    // and prepare indices
+    std::cout << "converting format...\n";
+    std::vector<Coordinate<int, D>> insert_keys(key_size);
+    std::memcpy(insert_keys.data(), input_coords.data(),
+                sizeof(int) * key_size * D);
+    std::vector<int> insert_vals(key_size);
+    std::iota(insert_vals.begin(), insert_vals.end(), 0);
 
-//     // convert raw data (at::Tensor) to std::vector
-//     // and prepare indices
-//     std::cout << "converting format...\n";
-//     std::vector<Coordinate<int, D>> insert_keys(key_size);
-//     std::memcpy(insert_keys.data(), input_coords.data(),
-//                 sizeof(int) * key_size * D);
-//     std::vector<int> insert_vals(key_size);
-//     std::iota(insert_vals.begin(), insert_vals.end(), 0);
+    // also make sure memcpy works correctly
+    for (int i = 0; i < key_size; ++i) {
+        for (int d = 0; d < D; ++d) {
+            assert(input_coords[i * D + d] == insert_keys[i][d]);
+        }
+    }
+    std::cout << "conversion finished\n";
 
-//     // also make sure memcpy works correctly
-//     for (int i = 0; i < key_size; ++i) {
-//         for (int d = 0; d < D; ++d) {
-//             assert(input_coords[i * D + d] == insert_keys[i][d]);
-//         }
-//     }
-//     std::cout << "conversion finished\n";
+    // cpu groundtruth
+    std::cout << "generating std::unordered_map ground truth hashtable...\n";
+    std::unordered_map<Coordinate<int, D>, int, CoordinateHashFunc<int, D>>
+            unordered_map;
+    for (int i = 0; i < key_size; ++i) {
+        unordered_map[insert_keys[i]] = insert_vals[i];
+    }
+    std::cout << "ground truth generated\n";
 
-//     // cpu groundtruth
-//     std::cout << "generating std::unordered_map ground truth hashtable...\n";
-//     std::unordered_map<Coordinate<int, D>, int, CoordinateHashFunc<int, D>>
-//             unordered_map;
-//     for (int i = 0; i < key_size; ++i) {
-//         unordered_map[insert_keys[i]] = insert_vals[i];
-//     }
-//     std::cout << "ground truth generated\n";
+    // gpu test
+    std::cout << "inserting to cuda::Hashmap...\n";
+    thrust::device_vector<Coordinate<int, D>> cuda_insert_keys = insert_keys;
+    thrust::device_vector<int> cuda_insert_vals = insert_vals;
+    cuda::Hashmap<Coordinate<int, D>, int> cuda_unordered_map(key_size);
+    cuda_unordered_map.Insert(cuda_insert_keys, cuda_insert_vals);
+    std::cout << "insertion finished\n";
 
-//     // gpu test
-//     std::cout << "inserting to cuda::Hashmap...\n";
-//     thrust::device_vector<Coordinate<int, D>> cuda_insert_keys = insert_keys;
-//     thrust::device_vector<int> cuda_insert_vals = insert_vals;
-//     cuda::Hashmap<Coordinate<int, D>, int> cuda_unordered_map(key_size);
-//     cuda_unordered_map.Insert(cuda_insert_keys, cuda_insert_vals);
-//     std::cout << "insertion finished\n";
+    // query
+    std::cout << "generating query_data...\n";
+    thrust::device_vector<Coordinate<int, D>> cuda_query_keys(
+            insert_keys.size());
+    for (int i = 0; i < key_size; ++i) {
+        if (i % 3 != 2) {  // 2/3 is valid
+            cuda_query_keys[i] = cuda_insert_keys[i];
+        } else {  // 1/3 is invalid
+            cuda_query_keys[i] = Coordinate<int, D>::random(generator, dist);
+        }
+    }
+    std::cout << "query data generated\n";
 
-//     // query
-//     std::cout << "generating query_data...\n";
-//     thrust::device_vector<Coordinate<int, D>> cuda_query_keys(insert_keys.size());
-//     for (int i = 0; i < key_size; ++i) {
-//         if (i % 3 != 2) {  // 2/3 is valid
-//             cuda_query_keys[i] = cuda_insert_keys[i];
-//         } else {  // 1/3 is invalid
-//             cuda_query_keys[i] = Coordinate<int, D>::random(generator, dist);
-//         }
-//     }
-//     std::cout << "query data generated\n";
+    std::cout << "query from cuda::Hashmap...\n";
+    auto cuda_query_results = cuda_unordered_map.Search(cuda_query_keys);
+    std::cout << "query results generated\n";
 
-//     std::cout << "query from cuda::Hashmap...\n";
-//     auto cuda_query_results = cuda_unordered_map.Search(cuda_query_keys);
-//     std::cout << "query results generated\n";
+    std::cout << "comparing query results against ground truth...\n";
+    for (int i = 0; i < cuda_query_keys.size(); ++i) {
+        auto iter = unordered_map.find(cuda_query_keys[i]);
+        if (iter == unordered_map.end()) {
+            assert(cuda_query_results.second[i] == 0);
+        } else {
+            iterator_t iterator = cuda_query_results.first[i];
+            Coordinate<int, D> key = *(thrust::device_ptr<Coordinate<int, D>>(
+                    (Coordinate<int, D>*)iterator));
+            int val = *(thrust::device_ptr<int>(
+                    (int*)(iterator + sizeof(Coordinate<int, D>))));
+            assert(key == cuda_query_keys[i]);
+            assert(val == iter->second);
+        }
+    }
 
-//     std::cout << "comparing query results against ground truth...\n";
-//     for (int i = 0; i < cuda_query_keys.size(); ++i) {
-//         auto iter = unordered_map.find(cuda_query_keys[i]);
-//         if (iter == unordered_map.end()) {
-//             assert(cuda_query_results.second[i] == 0);
-//         } else {
-//             Iterator<Coordinate<int, D>, int> iterator =
-//                     cuda_query_results.first[i];
-//             // Iterator == Pair*
-//             Pair<Coordinate<int, D>, int> kv =
-//                     *(thrust::device_ptr<Pair<Coordinate<int, D>, int>>(
-//                             iterator));
-//             assert(kv.first == cuda_query_keys[i]);
-//             assert(kv.second == iter->second);
-//         }
-//     }
-
-//     std::cout << "TEST_COORD_KEYS() passed\n";
-// }
+    std::cout << "TEST_COORD_KEYS() passed\n";
+}
 
 int main() {
     TEST_SIMPLE();
-    // TEST_6DIM_KEYS(1000000);
-    // TEST_COORD_KEYS(1000000);
+    TEST_6DIM_KEYS(1000000);
+    TEST_COORD_KEYS(1000000);
 }
