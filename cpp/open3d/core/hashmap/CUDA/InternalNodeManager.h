f7ed88dd6c2ab69db5444511e5b0d652c4837a17
Wei Dong <weidong@andrew.cmu.edu>
2020-09-27 23:43:46 -0400

    rename macros

--- cpp/open3d/core/hashmap/CUDA/InternalNodeManager.h
+++ cpp/open3d/core/hashmap/CUDA/InternalNodeManager.h
@@ -59,9 +59,11 @@ namespace core {
 /// InternalMemoryManager. Can be converted to a real ptr.
 /// \member next_slab_ptr:
 /// An internal ptr managed by InternalNodeManager.
+
+/// A slab is kWarpSize x kWarpSize bits, or kWarpSize 32-bit uints.
 class Slab {
 public:
-    addr_t kv_pair_ptrs[WARP_WIDTH - 1];
+    addr_t kv_pair_ptrs[kWarpSize - 1];
     addr_t next_slab_ptr;
 };
 
@@ -80,7 +82,8 @@ public:
     }
     __device__ __forceinline__ uint32_t* get_ptr_for_bitmap(
             const uint32_t super_block_idx, const uint32_t bitmap_idx) {
-        return super_blocks_ + super_block_idx * SUPER_BLOCK_SIZE_ + bitmap_idx;
+        return super_blocks_ + super_block_idx * kUIntsPerSuperBlock +
+               bitmap_idx;
     }
 
     // Objective: each warp selects its own memory_block warp allocator:
@@ -90,8 +93,8 @@ public:
 
         // loading the assigned memory block:
         memory_block_bitmap_ =
-                super_blocks_[super_block_index_ * SUPER_BLOCK_SIZE_ +
-                              memory_block_index_ * BITMAP_SIZE_ + lane_id];
+                super_blocks_[super_block_index_ * kUIntsPerSuperBlock +
+                              memory_block_index_ * kSlabsPerBlock + lane_id];
     }
 
     __device__ uint32_t WarpAllocate(const uint32_t& lane_id) {
@@ -105,15 +108,14 @@ public:
         int empty_lane = -1;
         uint32_t free_lane;
         uint32_t read_bitmap = memory_block_bitmap_;
-        // REVIEW: replace these 0xFFFFFFFF with values from Macros.h?
-        uint32_t allocated_result = 0xFFFFFFFF;
+        uint32_t allocated_result = kNotFoundFlag;
         // works as long as <31 bit are used in the allocated_result
         // in other words, if there are 32 super blocks and at most 64k blocks
         // per super block
 
-        while (allocated_result == 0xFFFFFFFF) {
+        while (allocated_result == kNotFoundFlag) {
             empty_lane = __ffs(~memory_block_bitmap_) - 1;
-            free_lane = __ballot_sync(0xFFFFFFFF, empty_lane >= 0);
+            free_lane = __ballot_sync(kSyncLanesMask, empty_lane >= 0);
             if (free_lane == 0) {
                 // all bitmaps are full: need to be rehashed again:
                 updateMemBlockIndex((threadIdx.x + blockIdx.x * blockDim.x) >>
@@ -124,19 +126,18 @@ public:
             uint32_t src_lane = __ffs(free_lane) - 1;
             if (src_lane == lane_id) {
                 read_bitmap = atomicCAS(
-                        super_blocks_ + super_block_index_ * SUPER_BLOCK_SIZE_ +
-                                memory_block_index_ * BITMAP_SIZE_ + lane_id,
+                        super_blocks_ +
+                                super_block_index_ * kUIntsPerSuperBlock +
+                                memory_block_index_ * kSlabsPerBlock + lane_id,
                         memory_block_bitmap_,
                         memory_block_bitmap_ | (1 << empty_lane));
                 if (read_bitmap == memory_block_bitmap_) {
                     // successful attempt:
                     memory_block_bitmap_ |= (1 << empty_lane);
-                    allocated_result = (super_block_index_
-                                        << SUPER_BLOCK_BIT_OFFSET_ALLOC_) |
-                                       (memory_block_index_
-                                        << MEM_BLOCK_BIT_OFFSET_ALLOC_) |
-                                       (lane_id << MEM_UNIT_BIT_OFFSET_ALLOC_) |
-                                       empty_lane;
+                    allocated_result =
+                            (super_block_index_ << kSuperBlockMaskBits) |
+                            (memory_block_index_ << kBlockMaskBits) |
+                            (lane_id << kSlabMaskBits) | empty_lane;
                 } else {
                     // Not successful: updating the current bitmap
                     memory_block_bitmap_ = read_bitmap;
@@ -144,7 +145,7 @@ public:
             }
             // asking for the allocated result;
             allocated_result =
-                    __shfl_sync(0xFFFFFFFF, allocated_result, src_lane);
+                    __shfl_sync(kSyncLanesMask, allocated_result, src_lane);
         }
         return allocated_result;
     }
@@ -153,8 +154,9 @@ public:
     // Since it is untouched, there shouldn't be any worries for the actual
     // memory contents to be reset again.
     __device__ void FreeUntouched(addr_t ptr) {
-        atomicAnd(super_blocks_ + getSuperBlockIndex(ptr) * SUPER_BLOCK_SIZE_ +
-                          getMemBlockIndex(ptr) * BITMAP_SIZE_ +
+        atomicAnd(super_blocks_ +
+                          getSuperBlockIndex(ptr) * kUIntsPerSuperBlock +
+                          getMemBlockIndex(ptr) * kSlabsPerBlock +
                           (getMemUnitIndex(ptr) >> 5),
                   ~(1 << (getMemUnitIndex(ptr) & 0x1F)));
     }
@@ -162,16 +164,16 @@ public:
 private:
     __device__ __host__ __forceinline__ uint32_t
     getSuperBlockIndex(addr_t address) const {
-        return address >> SUPER_BLOCK_BIT_OFFSET_ALLOC_;
+        return address >> kSuperBlockMaskBits;
     }
     __device__ __host__ __forceinline__ uint32_t
     getMemBlockIndex(addr_t address) const {
-        return ((address >> MEM_BLOCK_BIT_OFFSET_ALLOC_) & 0x1FFFF);
+        return ((address >> kBlockMaskBits) & 0x1FFFF);
     }
     __device__ __host__ __forceinline__ addr_t
     getMemBlockAddress(addr_t address) const {
-        return (MEM_BLOCK_OFFSET_ +
-                getMemBlockIndex(address) * MEM_BLOCK_SIZE_);
+        return (kBitmapsPerSuperBlock +
+                getMemBlockIndex(address) * kUIntsPerBlock);
     }
     __device__ __host__ __forceinline__ uint32_t
     getMemUnitIndex(addr_t address) const {
@@ -179,35 +181,34 @@ private:
     }
     __device__ __host__ __forceinline__ addr_t
     getMemUnitAddress(addr_t address) {
-        return getMemUnitIndex(address) * MEM_UNIT_SIZE_;
+        return getMemUnitIndex(address) * kWarpSize;
     }
 
     // called at the beginning of the kernel:
     __device__ void createMemBlockIndex(uint32_t global_warp_id) {
-        super_block_index_ = global_warp_id % NUM_SUPER_BLOCKS_;
-        memory_block_index_ =
-                (hash_coef_ * global_warp_id) >> (32 - LOG_NUM_MEM_BLOCKS_);
+        super_block_index_ = global_warp_id % kSuperBlocks;
+        memory_block_index_ = (hash_coef_ * global_warp_id) >>
+                              (32 - kBlocksPerSuperBlockInBits);
     }
 
     // called when the allocator fails to find an empty unit to allocate:
     __device__ void updateMemBlockIndex(uint32_t global_warp_id) {
         num_attempts_++;
         super_block_index_++;
-        super_block_index_ = (super_block_index_ == NUM_SUPER_BLOCKS_)
-                                     ? 0
-                                     : super_block_index_;
+        super_block_index_ =
+                (super_block_index_ == kSuperBlocks) ? 0 : super_block_index_;
         memory_block_index_ = (hash_coef_ * (global_warp_id + num_attempts_)) >>
-                              (32 - LOG_NUM_MEM_BLOCKS_);
+                              (32 - kBlocksPerSuperBlockInBits);
         // loading the assigned memory block:
         memory_block_bitmap_ =
-                *((super_blocks_ + super_block_index_ * SUPER_BLOCK_SIZE_) +
-                  memory_block_index_ * BITMAP_SIZE_ + (threadIdx.x & 0x1f));
+                *((super_blocks_ + super_block_index_ * kUIntsPerSuperBlock) +
+                  memory_block_index_ * kSlabsPerBlock + (threadIdx.x & 0x1f));
     }
 
     __host__ __device__ addr_t addressDecoder(addr_t address_ptr_index) {
-        return getSuperBlockIndex(address_ptr_index) * SUPER_BLOCK_SIZE_ +
+        return getSuperBlockIndex(address_ptr_index) * kUIntsPerSuperBlock +
                getMemBlockAddress(address_ptr_index) +
-               getMemUnitIndex(address_ptr_index) * WARP_SIZE;
+               getMemUnitIndex(address_ptr_index) * kWarpSize;
     }
 
     __host__ __device__ void print_address(addr_t address_ptr_index) {
@@ -247,21 +248,20 @@ public:
 
         // In the light version, we put num_super_blocks super blocks within
         // a single array
-        gpu_context_.super_blocks_ = static_cast<uint32_t*>(
-                MemoryManager::Malloc(SUPER_BLOCK_SIZE_ * NUM_SUPER_BLOCKS_ *
-                                              sizeof(uint32_t),
-                                      device_));
+        gpu_context_.super_blocks_ =
+                static_cast<uint32_t*>(MemoryManager::Malloc(
+                        kUIntsPerSuperBlock * kSuperBlocks * sizeof(uint32_t),
+                        device_));
 
         OPEN3D_CUDA_CHECK(cudaMemset(
                 gpu_context_.super_blocks_, 0xFF,
-                SUPER_BLOCK_SIZE_ * NUM_SUPER_BLOCKS_ * sizeof(uint32_t)));
+                kUIntsPerSuperBlock * kSuperBlocks * sizeof(uint32_t)));
 
-        for (uint32_t i = 0; i < NUM_SUPER_BLOCKS_; i++) {
+        for (uint32_t i = 0; i < kSuperBlocks; i++) {
             // setting bitmaps into zeros:
             OPEN3D_CUDA_CHECK(cudaMemset(
-                    gpu_context_.super_blocks_ + i * SUPER_BLOCK_SIZE_, 0x00,
-                    NUM_MEM_BLOCKS_PER_SUPER_BLOCK_ * BITMAP_SIZE_ *
-                            sizeof(uint32_t)));
+                    gpu_context_.super_blocks_ + i * kUIntsPerSuperBlock, 0x00,
+                    kBlocksPerSuperBlock * kSlabsPerBlock * sizeof(uint32_t)));
         }
     }
 
@@ -270,16 +270,17 @@ public:
     }
 
     std::vector<int> CountSlabsPerSuperblock() {
-        const uint32_t num_super_blocks = NUM_SUPER_BLOCKS_;
+        const uint32_t num_super_blocks = kSuperBlocks;
 
-        thrust::device_vector<uint32_t> slabs_per_superblock(NUM_SUPER_BLOCKS_);
+        thrust::device_vector<uint32_t> slabs_per_superblock(kSuperBlocks);
         thrust::fill(slabs_per_superblock.begin(), slabs_per_superblock.end(),
                      0);
 
         // counting total number of allocated memory units:
-        int num_mem_units = NUM_MEM_BLOCKS_PER_SUPER_BLOCK_ * 32;
-        int num_cuda_blocks = (num_mem_units + BLOCKSIZE_ - 1) / BLOCKSIZE_;
-        CountSlabsPerSuperblockKernel<<<num_cuda_blocks, BLOCKSIZE_>>>(
+        int num_mem_units = kBlocksPerSuperBlock * 32;
+        int num_cuda_blocks =
+                (num_mem_units + kThreadsPerBlock - 1) / kThreadsPerBlock;
+        CountSlabsPerSuperblockKernel<<<num_cuda_blocks, kThreadsPerBlock>>>(
                 gpu_context_,
                 thrust::raw_pointer_cast(slabs_per_superblock.data()));
         OPEN3D_CUDA_CHECK(cudaDeviceSynchronize());
@@ -301,12 +302,12 @@ __global__ void CountSlabsPerSuperblockKernel(
         InternalNodeManagerContext context, uint32_t* slabs_per_superblock) {
     uint32_t tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-    int num_bitmaps = NUM_MEM_BLOCKS_PER_SUPER_BLOCK_ * 32;
+    int num_bitmaps = kBlocksPerSuperBlock * 32;
     if (tid >= num_bitmaps) {
         return;
     }
 
-    for (uint32_t i = 0; i < NUM_SUPER_BLOCKS_; i++) {
+    for (uint32_t i = 0; i < kSuperBlocks; i++) {
         uint32_t read_bitmap = *(context.get_ptr_for_bitmap(i, tid));
         atomicAdd(&slabs_per_superblock[i], __popc(read_bitmap));
     }
