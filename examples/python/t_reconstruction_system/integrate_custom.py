7a132867c29036039544095c248fd9df97556921
Wei Dong <thended@gmail.com>
2022-02-11 10:19:23 -0500

    update example

--- examples/python/t_reconstruction_system/integrate_custom.py
+++ examples/python/t_reconstruction_system/integrate_custom.py
@@ -1,56 +1,78 @@
-# Open3D: www.open3d.org
+# ----------------------------------------------------------------------------
+# -                        Open3D: www.open3d.org                            -
+# ----------------------------------------------------------------------------
 # The MIT License (MIT)
-# See license file or visit www.open3d.org for details
+#
+# Copyright (c) 2018-2021 www.open3d.org
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+# ----------------------------------------------------------------------------
+
+# examples/python/t_reconstruction_system/ray_casting.py
+
+# P.S. This example is used in documentation, so, please ensure the changes are
+# synchronized.
 
 import os
 import numpy as np
 import open3d as o3d
+import open3d.core as o3c
 import time
 import matplotlib.pyplot as plt
 
 from config import ConfigParser
-from common import load_image_file_names, save_poses, load_intrinsic, load_extrinsics
-import imageio
-
-import torch
-from torch.utils.dlpack import from_dlpack
+from common import load_rgbd_file_names, load_depth_file_names, save_poses, load_intrinsic, load_extrinsics, get_default_testdata
 
 from tqdm import tqdm
 
 
-def to_torch(o3d_tensor):
-    return from_dlpack(o3d_tensor.to_dlpack())
-
-
 def integrate(depth_file_names, color_file_names, intrinsic, extrinsics,
               config):
-    if os.path.exists(config.npz_file):
+    if os.path.exists(config.path_npz):
         print('Voxel block grid npz file {} found, trying to load...'.format(
-            config.npz_file))
-        vbg = o3d.t.geometry.VoxelBlockGrid.load(config.npz_file)
+            config.path_npz))
+        vbg = o3d.t.geometry.VoxelBlockGrid.load(config.path_npz)
         print('Loading finished.')
     else:
         print('Voxel block grid npz file {} not found, trying to integrate...'.
-              format(config.npz_file))
+              format(config.path_npz))
 
-        n_files = len(color_file_names)
+        n_files = len(depth_file_names)
         device = o3d.core.Device(config.device)
 
         voxel_size = 3.0 / 512
         trunc = voxel_size * 4
         res = 8
 
-        vbg = o3d.t.geometry.VoxelBlockGrid(
-            ('tsdf', 'weight'),
-            (o3d.core.Dtype.Float32, o3d.core.Dtype.Float32), ((1), (1)),
-            voxel_size, res, 100000, o3d.core.Device('CUDA:0'))
+        if config.integrate_color:
+            vbg = o3d.t.geometry.VoxelBlockGrid(
+                ('tsdf', 'weight', 'color'),
+                (o3c.float32, o3c.float32, o3c.float32), ((1), (1), (3)),
+                3.0 / 512, 8, 100000, device)
+        else:
+            vbg = o3d.t.geometry.VoxelBlockGrid(
+                ('tsdf', 'weight'), (o3c.float32, o3c.float32), ((1), (1)),
+                3.0 / 512, 8, 100000, device)
 
         start = time.time()
         for i in tqdm(range(n_files)):
-            print('Integrating frame {}/{}'.format(i, n_files))
-
             depth = o3d.t.io.read_image(depth_file_names[i]).to(device)
-            color = o3d.t.io.read_image(color_file_names[i]).to(device)
             extrinsic = extrinsics[i]
 
             start = time.time()
@@ -63,35 +85,29 @@ def integrate(depth_file_names, color_file_names, intrinsic, extrinsics,
 
             # Find buf indices in the underlying engine
             buf_indices, masks = vbg.hashmap().find(frustum_block_coords)
-            torch.cuda.synchronize()
+            o3d.core.cuda.synchronize()
             end = time.time()
-            print('hash map preparation: {}'.format(end - start))
 
             start = time.time()
             voxel_coords, voxel_indices = vbg.voxel_coordinates_and_flattened_indices(
                 buf_indices)
-            torch.cuda.synchronize()
+            o3d.core.cuda.synchronize()
             end = time.time()
-            print('enumerate voxels: {}'.format(end - start))
 
             # Now project them to the depth and find association
             # (3, N) -> (2, N)
             start = time.time()
-            extrinsic_dev = extrinsic.to(device, o3d.core.Dtype.Float32)
+            extrinsic_dev = extrinsic.to(device, o3c.float32)
             xyz = extrinsic_dev[:3, :3] @ voxel_coords.T() + extrinsic_dev[:3,
                                                                            3:]
 
-            # o3d.visualization.draw(
-            #     [o3d.t.geometry.PointCloud(xyz.T())])
-
-            intrinsic_dev = intrinsic.to(device, o3d.core.Dtype.Float32)
+            intrinsic_dev = intrinsic.to(device, o3c.float32)
             uvd = intrinsic_dev @ xyz
             d = uvd[2]
-            u = (uvd[0] / d).round().to(o3d.core.Dtype.Int64)
-            v = (uvd[1] / d).round().to(o3d.core.Dtype.Int64)
-            torch.cuda.synchronize()
+            u = (uvd[0] / d).round().to(o3c.int64)
+            v = (uvd[1] / d).round().to(o3c.int64)
+            o3d.core.cuda.synchronize()
             end = time.time()
-            print('geometry transformation: {}'.format(end - start))
 
             start = time.time()
             mask_proj = (d > 0) & (u >= 0) & (v >= 0) & (u < depth.columns) & (
@@ -101,9 +117,7 @@ def integrate(depth_file_names, color_file_names, intrinsic, extrinsics,
             u_proj = u[mask_proj]
             d_proj = d[mask_proj]
             depth_readings = depth.as_tensor()[v_proj, u_proj, 0].to(
-                o3d.core.Dtype.Float32) / config.depth_scale
-            color_readings = color.as_tensor()[v_proj, u_proj].to(
-                o3d.core.Dtype.Float32)
+                o3c.float32) / config.depth_scale
             sdf = depth_readings - d_proj
 
             mask_inlier = (depth_readings > 0) \
@@ -112,13 +126,11 @@ def integrate(depth_file_names, color_file_names, intrinsic, extrinsics,
 
             sdf[sdf >= trunc] = trunc
             sdf = sdf / trunc
-            torch.cuda.synchronize()
+            o3d.core.cuda.synchronize()
             end = time.time()
-            print('association: {}'.format(end - start))
 
             start = time.time()
             weight = vbg.attribute('weight').reshape((-1, 1))
-            # color = vbg.attribute('color').reshape((-1, 3))
             tsdf = vbg.attribute('tsdf').reshape((-1, 1))
 
             valid_voxel_indices = voxel_indices[mask_proj][mask_inlier]
@@ -128,22 +140,29 @@ def integrate(depth_file_names, color_file_names, intrinsic, extrinsics,
             tsdf[valid_voxel_indices] \
                 = (tsdf[valid_voxel_indices] * w +
                    sdf[mask_inlier].reshape(w.shape)) / (wp)
-            # color[valid_voxel_indices] \
-            #     = (color[valid_voxel_indices] * w +
-            #              color_readings[mask_inlier]) / (wp)
-            weight[valid_voxel_indices] = wp
-            torch.cuda.synchronize()
-            end = time.time()
-            print('update: {}'.format(end - start))
+            if config.integrate_color:
+                color = o3d.t.io.read_image(color_file_names[i]).to(device)
+                color_readings = color.as_tensor()[v_proj,
+                                                   u_proj].to(o3c.float32)
 
-        print('Saving to {}...'.format(config.npz_file))
-        vbg.save(config.npz_file)
+                color = vbg.attribute('color').reshape((-1, 3))
+                color[valid_voxel_indices] \
+                    = (color[valid_voxel_indices] * w +
+                             color_readings[mask_inlier]) / (wp)
+
+            weight[valid_voxel_indices] = wp
+            o3d.core.cuda.synchronize()
+            end = time.time()
+
+        print('Saving to {}...'.format(config.path_npz))
+        vbg.save(config.path_npz)
         print('Saving finished')
 
     return vbg
 
 
 if __name__ == '__main__':
+
     parser = ConfigParser()
     parser.add(
         '--config',
@@ -151,14 +170,25 @@ if __name__ == '__main__':
         help='YAML config file path. Please refer to default_config.yml as a '
         'reference. It overrides the default config file, but will be '
         'overridden by other command line inputs.')
+    parser.add('--integrate_color', action='store_true')
     parser.add('--path_trajectory',
                help='path to the trajectory .log or .json file.')
-    parser.add('--npz_file',
+    parser.add('--path_npz',
                help='path to the npz file that stores voxel block grid.',
                default='vbg.npz')
     config = parser.get_config()
 
-    depth_file_names, color_file_names = load_image_file_names(config)
+    if config.path_dataset == '':
+        config.path_dataset = get_default_testdata()
+        config.path_trajectory = os.path.join(config.path_dataset,
+                                              'trajectory.log')
+
+    if config.integrate_color:
+        depth_file_names, color_file_names = load_rgbd_file_names(config)
+    else:
+        depth_file_names = load_depth_file_names(config)
+        color_file_names = None
+
     intrinsic = load_intrinsic(config)
     extrinsics = load_extrinsics(config.path_trajectory, config)
 
